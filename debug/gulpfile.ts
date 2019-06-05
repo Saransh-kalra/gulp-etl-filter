b370f94e81b267ebef2ceb4853ef5666f30dbb10
Unknown <ksaransh201@gmail.com>
2019-06-05 10:22:14 -0500

    got rid of target and tap csv

--- debug/gulpfile.ts
+++ debug/gulpfile.ts
@@ -4,8 +4,8 @@ changed names */
 
 import { src, dest } from 'gulp'
 import * as rename from 'gulp-rename'
-import { tapCsv } from 'gulp-etl-tap-csv'
-import { targetCsv } from 'gulp-etl-target-csv'
+//import { tapCsv } from 'gulp-etl-tap-csv'
+//import { targetCsv } from 'gulp-etl-target-csv'
 const errorHandler = require('gulp-error-handle'); // handle all errors in one handler, but still stop the stream if there are errors
 
 import {log} from '../src/log'
@@ -14,7 +14,7 @@ import {MyPlugin} from '../src/plugin'
 export function processCsv(callback: any) {
     try {    
         //return conn.src(['tap/*.csv'], {base: '.',buffer: false})
-        return src(['testdata/proccessed/*.csv','!testdata/proccessed/*-parsed.csv'], {buffer: true})
+        return src(['../testdata/*.ndjson'], {buffer: true})
         .pipe(errorHandler(function(err:any) {
             console.error('oops: ' + err)
             callback(err)
@@ -22,19 +22,19 @@ export function processCsv(callback: any) {
         .on('data', function (file:any) {
             console.log('TypeScript: Starting processing on ' + file.basename)
         })  
-        .pipe(tapCsv({ columns:true }))
+        //.pipe(tapCsv({ columns:true }))
         .pipe(rename({ extname: ".ndjson" })) // rename to *.ndjson
         .on('data', function (file:any) {
             console.log('Done parsing ' + file.basename)
         })  
         .pipe(MyPlugin())
-        .pipe(targetCsv({header:true}))
-        .pipe(rename({suffix:"-parsed", extname: ".csv" })) // rename to *.ndjson
+        //.pipe(targetCsv({header:true}))
+        .pipe(rename({suffix:"-parsed", extname: ".ndjson" })) // rename to *.ndjson
         .on('data', function (file:any) {
             console.log('Done processing on ' + file.basename)
         })
         .pipe(log('gulpfile'))  
-        .pipe(dest('testdata/proccessed/'));
+        .pipe(dest('../testdata/processed/'));
     }
     catch (err) {
         console.error(err)    
